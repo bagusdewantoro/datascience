363d411a86c9495464cad8551ae84b6adfceeee5
Bagus Dewantoro <bagusdewantoro86@gmail.com>
2021-01-14 13:13:47 +0700

    friendship

--- 1_friendship.py
+++ 1_friendship.py
@@ -15,6 +15,8 @@ users = [
 friendship_pairs = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
                     (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
 
+print("\nUser ID and name:") 
+for user in users: print(user) 
 
 #======== DATA INTEGRITY =========================================
 # Initialize the dict with an empty list for each user id:
@@ -25,7 +27,8 @@ for i, j in friendship_pairs:
     friendships[i].append(j)  # Add j as a friend of user i
     friendships[j].append(i)  # Add i as a friend of user j
 
-print(friendships)
+print("\nFriendship pairs:")
+for friends in friendships: print(friends, ":", friendships[friends])
 
 
 #======== IDENTIFY MOST CONNECTED USER =========================================
@@ -37,28 +40,29 @@ def number_of_friends(user):
     return len(friend_ids)
 
 total_connections = sum(number_of_friends(user) for user in users)
-print(total_connections)
+print("\nTotal connection = ", total_connections)
 
 num_users = len(users)                            # length of the users list
 avg_connections = total_connections / num_users   # 24 / 10 == 2.4
-print(avg_connections)
+print("\nAverage connection = ", avg_connections)
 
 # # bikin list yang terdiri dari ("id user", "jumlah teman")
 num_friends_by_id = [(user["id"], number_of_friends(user)) for user in users]
-print(num_friends_by_id)
+print("\nNumber of friends by id (\"id\", \"num. of friends\") : \n", num_friends_by_id)
 
 # urutkan list di atas berdasarkan "jumlah teman".. x[1]
 num_friends_by_id.sort(                                # list yang mau di-sort
        key=lambda x : x[1],   # diurutkan oleh "jumlah teman"
        reverse=True)                                   # largest to smallest
-print(num_friends_by_id)
+print("\nSorted list above : \n", num_friends_by_id)
 
 
 #======== SUGGEST 'USER YOU MAY KNOW' =========================================
-def foaf_ids_bad(user):
-    """foaf is short for "friend of a friend" """
-    return [foaf_id
-            for friend_id in friendships[user["id"]]
-            for foaf_id in friendships[friend_id]]
+def friends_of_friend(daftar):
+    return [anggota
+            for teman in friendships[daftar["id"]]
+            for anggota in friendships[teman]]
 
-print(foaf_ids_bad(users[0]))
+print(f"\nPeople that {users[0]} may knows : ", friends_of_friend(users[0]))
+print(f"People that {users[3]} may knows : ", friends_of_friend(users[3]))
+print(f"People that {users[9]} may knows : ", friends_of_friend(users[9]))
