dc64550d35792caab4b184943f3574ce3c2bca77
Bagus Dewantoro <bagusdewantoro86@gmail.com>
2021-01-23 15:17:47 +0700

    friendships

--- 1_friendship.py
+++ 1_friendship.py
@@ -40,21 +40,21 @@ def number_of_friends(user):
     return len(friend_ids)
 
 total_connections = sum(number_of_friends(user) for user in users)
-print("\nTotal connection = ", total_connections)
+# print("\nTotal connection = ", total_connections)
 
 num_users = len(users)                            # length of the users list
 avg_connections = total_connections / num_users   # 24 / 10 == 2.4
-print("\nAverage connection = ", avg_connections)
+# print("\nAverage connection = ", avg_connections)
 
 # # bikin list yang terdiri dari ("id user", "jumlah teman")
 num_friends_by_id = [(user["id"], number_of_friends(user)) for user in users]
-print("\nNumber of friends by id (\"id\", \"num. of friends\") : \n", num_friends_by_id)
+# print("\nNumber of friends by id (\"id\", \"num. of friends\") : \n", num_friends_by_id)
 
 # urutkan list di atas berdasarkan "jumlah teman".. x[1]
 num_friends_by_id.sort(                                # list yang mau di-sort
        key=lambda x : x[1],             # diurutkan oleh "jumlah teman"
        reverse=True)                                   # largest to smallest
-print("\nSorted list above : \n", num_friends_by_id)
+# print("\nSorted list above : \n", num_friends_by_id)
 
 
 #======== SUGGEST 'USER YOU MAY KNOW' =========================================
@@ -79,6 +79,7 @@ def friends_of_friend2(daftar):
 print(f"\nPeople that {users[0]} may knows : ", friends_of_friend2(users[0]))
 print(f"People that {users[3]} may knows : ", friends_of_friend2(users[3]))
 print(f"People that {users[9]} may knows : ", friends_of_friend2(users[9]))
+
 
 #======== COUNT OF MUTUAL FRIENDS =========================================
 from collections import Counter
@@ -111,3 +112,65 @@ def mutual_friends_number2(daftar):
 mutual3 = mutual_friends_number2(users[3])
 print("Friends of a friends of '3' is:", mutual3)
 print("Count mutual friends of '3' :", Counter(mutual3))
+
+
+#======== USER INTERESTS =========================================
+hobi = [
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
+    (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "theory"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
+    ]
+
+# cek orang yang suka sama hobi tertentu
+
+# cara 1 :
+def orang_yang_suka(target_hobi):
+    return [anggota
+            for anggota, pilihan_hobi in hobi
+            if pilihan_hobi == target_hobi]
+sukaJava = orang_yang_suka("Java")
+sukaPython = orang_yang_suka("Python")
+print("Orang yang suka Java:", sukaJava)
+print("Orang yang suka Python:", sukaPython)
+
+# cara 2 :
+def orang_yang_suka2(target_hobi):
+    a = []
+    for anggota, pilihan_hobi in hobi:
+        if pilihan_hobi == target_hobi:
+            a.append(anggota)
+    return a
+sukaJava2 = orang_yang_suka2("Java")
+sukaPython2 = orang_yang_suka2("Python")
+print("Orang yang suka Java:", sukaJava2)
+print("Orang yang suka Python:", sukaPython2)
+
+from collections import defaultdict
+
+anggota_berdasarkan_hobi = defaultdict(list)
+for anggota, pilihan_hobi in hobi:
+    anggota_berdasarkan_hobi[pilihan_hobi].append(anggota)
+print("\nData ID anggota berdasarkan Pilihan Hobi:")
+print(anggota_berdasarkan_hobi)
+
+hobi_berdasarkan_anggota = defaultdict(list)
+for anggota, pilihan_hobi in hobi:
+    hobi_berdasarkan_anggota[anggota].append(pilihan_hobi)
+print("\nData Pilihan hobi berdasarkan ID anggota:")
+print(hobi_berdasarkan_anggota)
+
+#def hobi_paling_disuka(daftar):
+#    return Counter(a
+#                for pilihan_hobi in
+#    )
