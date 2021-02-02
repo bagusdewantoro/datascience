95b73b36e5ace05f8985e5d4905ea08d4fdd427f
Bagus Dewantoro <bagusdewantoro86@gmail.com>
2021-02-02 16:22:07 +0700

    matrices

--- 4_matrices.py
+++ 4_matrices.py
@@ -37,3 +37,14 @@ def nilai_di_kolom(A: Matrix, j: int) -> Vector:
 
 print("\nNilai di baris ke-0 pada Matrix A:", nilai_di_baris(A, 0))
 print("Nilai di kolom ke-1 pada Matrix B:", nilai_di_kolom(B, 1))
+
+# =============== MEMBUAT MATRIX DARI DENGAN SHAPE TERTENTU =====================
+from typing import Callable
+
+def buat_matrix(jml_baris: int, jml_kolom: int, masukkan: Callable[[int, int], str]) -> Matrix:
+    return [[masukkan(i, j) 
+        for j in range(jml_kolom)]
+        for i in range(jml_baris)]
+
+print("\nMatrix 3 x 5 dengan nilai diagonal = 5 dan selain itu 0'")
+print(buat_matrix(3, 5, lambda x, y: 5 if x == y else 0))
